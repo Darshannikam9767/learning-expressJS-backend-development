8031206db9fb3bb7659b8a79366c6217546a2c84
Darshannikam9767 <darshannikam64@gmail.com>
2026-06-09 15:16:22 +0530

    implemented mongoDB and models

--- xpress_generator/demo/routes/index.js
+++ xpress_generator/demo/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/create", async(req, res)=>{
+  const createdUser = await userModel.create({
+    
+    username:"dummy",
+    name:"Darshan",
+    age:21
+
+   })
+
+   return res.json({createdUser:`${createdUser}`})
+})
+
+router.get("/find/allusers",async(req,res)=>{
+  let allUsers = await userModel.find()
+  return res.json({allUsers : allUsers})
+})
+
+router.get("/find/oneuser",async(req,res)=>{
+  let user = await userModel.findOne({name:"Darshan"})
+  return res.json({user:user})
+})
+
+router.get("/delete/oneuser",async (req,res) => {
+  let deletedUser = await userModel.findOneAndDelete({
+    name:"Darshan"
+  })  
+
+  return res.json({deletedUser:deletedUser})
+})
 
 module.exports = router;
